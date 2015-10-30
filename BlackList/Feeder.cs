07a1d6ce3d1dab8b894715e32f676905666b60f8
moimran <postme.imran@gmail.com>
2015-10-30 23:07:58 +0530

    csv error removed

--- BlackList/Feeder.cs
+++ BlackList/Feeder.cs
@@ -16,17 +16,17 @@ namespace BlacklistNew
         Dictionary<string, List<KeyValuePair<string, List<string>>>> BlackListUnique = new Dictionary<string, List<KeyValuePair<string, List<string>>>>();
         internal List<IExtractor> FeedList = new List<IExtractor>();
         List<string> dup = new List<string>();
-        //Ipset Ipsetdb = new Ipset();
-        ShunList shunDb = new ShunList();
-        BlockList_de BlockDb = new BlockList_de();
-        SSLIpBlackList ccList = new SSLIpBlackList();
+        Ipset Ipsetdb = new Ipset();
+        //ShunList shunDb = new ShunList();
+        //BlockList_de BlockDb = new BlockList_de();
+        //SSLIpBlackList ccList = new SSLIpBlackList();
         //BadIp badip = new BadIp();
         public void Runner()
         {
-            //FeedList.Add(Ipsetdb);
-            FeedList.Add(shunDb);
-            FeedList.Add(BlockDb);
-            FeedList.Add(ccList);
+            FeedList.Add(Ipsetdb);
+            //FeedList.Add(shunDb);
+            //FeedList.Add(BlockDb);
+            //FeedList.Add(ccList);
             //FeedList.Add(badip);
             foreach (IExtractor item in FeedList)
             {
@@ -45,7 +45,6 @@ namespace BlacklistNew
                     string ip = item.Key;
                     if (!BlackListUnique.ContainsKey(ip))
                     {
-                        //BlackListUnique[ip] = item.Value;
                         BlackListUnique.Add(ip, item.Value);
                     }
                     else
@@ -77,8 +76,13 @@ namespace BlacklistNew
             }
             Console.WriteLine("ToTal Number of BlackList IP's = {0}", BlackListUnique.Count());
             Console.WriteLine("ToTal Number of duplicates IP's Found = {0}", dup.Count());
- 
-            using (StreamWriter sw = File.CreateText("blacklistDB1.csv"))
+
+            CsvBlackListDB();
+        }
+
+        private void CsvBlackListDB()
+        {
+            using (StreamWriter sw = File.CreateText("blacklistDB1.tsv"))
             {
                 List<string> tempCat = new List<string>();
                 List<string> tempDesc = new List<string>();
@@ -87,20 +91,15 @@ namespace BlacklistNew
                 Console.WriteLine("Writing to File....");
                 foreach (KeyValuePair<string, List<KeyValuePair<string, List<string>>>> item in BlackListUnique)
                 {
-                    //Console.WriteLine("zzzzzzzzzzzzzzzzzzzzzzzzzzzzzz");
                     string ip = item.Key;
-                    //Console.WriteLine("IP = {0}", ip);
                     for (int i = 0; i < item.Value.Count; i++)
                     {
-                        //Console.WriteLine("============================");
-                        //Console.WriteLine("{0}", item.Value.ElementAt(i).Key);
                         foreach (var item2 in item.Value.ElementAt(i).Value)
                         {
                             if (item.Value.ElementAt(i).Key == "category")
                             {
                                 tempCat.Add(item2);
                             }
-                            //Console.WriteLine(item2);
                             if (item.Value.ElementAt(i).Key == "description")
                             {
                                 tempDesc.Add(item2);
@@ -113,15 +112,15 @@ namespace BlacklistNew
                             {
                                 tempUrl.Add(item2);
                             }
-                            
+
                         }
                     }
-                    string a = string.Join("  ", tempCat);
-                    string b = string.Join("  ", tempDesc);
-                    string c = string.Join("  ", tempDate);
-                    string d = string.Join("  ", tempUrl);
+                    string a = string.Join("||", tempCat);
+                    string b = string.Join("||", tempDesc);
+                    string c = string.Join("||", tempDate);
+                    string d = string.Join("||", tempUrl);
                     //Console.WriteLine(ip + "," + a + "," + b + "," + c + "," + d);
-                    sw.WriteLine(ip + "," + a + "," + b + "," + c + "," + d);
+                    sw.WriteLine(ip + "\t" + a + "\t" + b + "\t" + c + "\t" + d);
                     tempCat = new List<string>();
                     tempDesc = new List<string>();
                     tempDate = new List<string>();
--- BlackList/Ipset.cs
+++ BlackList/Ipset.cs
@@ -11,7 +11,7 @@ namespace BlacklistNew
 {
     class Ipset : Extractor
     {
-        
+
 
         private Dictionary<string, string> FileUrl = new Dictionary<string, string>();
         public override Dictionary<string, List<KeyValuePair<string, List<string>>>> ExtractIP()
@@ -30,7 +30,7 @@ namespace BlacklistNew
         }
         internal void IPsetDB(FileInfo mfile)
         {
-            
+
             string path = Common.GetPath();
             FileUrl = Common.GetURL();// Creates a dictionary of Filename and URL from which it got downloaded
             using (var rd = new StreamReader(path + mfile.ToString()))
@@ -44,25 +44,55 @@ namespace BlacklistNew
                 string IpDescription = "";
                 string category = "";
                 string IpDate = "";
+                string url = "";
+                List<string> tempDesc = new List<string>();
+                int i = 1;
+                string t = "";
+                string l = "";
+                int k = 0;
                 while (!rd.EndOfStream)
                 {
-                    string x = rd.ReadLine();
-                    IpDescription = x.Contains("#") ? IpDescription + x + "\n" : IpDescription;
+                    string x=rd.ReadLine();
+                    if (i != k && k==0)
+                    {
+                        if (i >= 6)
+                        {
+                            if (!x.Contains("Maintainer"))
+                            {
+                                tempDesc.Add(x);                              
+                            }
+                            else
+                            {
+                                k = i;
+                                k++;
+                                l = string.Join(" ", tempDesc);
+                                l = l.Replace("#", " ");
+                                IpDescription=l;
+                            }
+                        }
+                    }
+                    i++;
+                    if (i==k)
+                    {
+                        k = 1;
+                    }
                     category = x.Contains("Category") ? x.Split(':')[1].ToString().Trim() : category;
                     IpDate = x.Contains("Source File Date") ? x.Split(':')[1].ToString().Trim() : IpDate;
+
+                    url = x.Contains("List source URL") ? x.Split(new char[] {':'}, 2, StringSplitOptions.RemoveEmptyEntries)[1].ToString().Trim() : url;
                     var match = Regex.Match(x, @"\b(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})\b");
                     if (match.ToString() != "")
                     {
                         IP = match.ToString().Trim();
                         DescriptionList.Add(IpDescription);
                         CategoryList.Add(category);
-                        UrlList.Add(FileUrl[mfile.ToString()]);
+                        UrlList.Add(url);
                         IpDateList.Add(IpDate);
                         InfoList.Add(new KeyValuePair<string, List<string>>(fieldC, CategoryList));
                         InfoList.Add(new KeyValuePair<string, List<string>>(fieldD, DescriptionList));
                         InfoList.Add(new KeyValuePair<string, List<string>>(fieldU, UrlList));
                         InfoList.Add(new KeyValuePair<string, List<string>>(fieldT, IpDateList));
-                        
+
                         if (!BlackListDB.ContainsKey(IP))
                         {
                             totalip.Add(IP);
