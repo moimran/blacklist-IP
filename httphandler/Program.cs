ab3bf88dec6cc925577124fcde3ffd26de61a181
moimran <postme.imran@gmail.com>
2015-10-30 12:42:42 +0530

    removed unwanted folders

--- httphandler/Program.cs
+++ httphandler/Program.cs
@@ -23,53 +23,12 @@ namespace BlackListFeed
                     if (line != null)
                     {
                         UrlList.Add(line.Trim());
-                        //Console.WriteLine(line); // Use line.
                     }
                 }
 
             }
-            //UrlList.ForEach(Console.WriteLine);
-            //List<string> UrlList = new List<string> { "http://autoshun.org/files/shunlist.csv", "https://www.packetmail.net/iprep.txt", "http://atrack.h3x.eu/api/asprox_active_csv.php", "http://malc0de.com/bl/IP_Blacklist.txt" };
-            //List<string> UrlList = new List<string> { "https://raw.githubusercontent.com/ktsaou/blocklist-ipsets/master/alienvault_reputation.ipset" };
             MultipleFeeds feeds = new MultipleFeeds(UrlList);
             feeds.StartTasks();
-            //string path = Common.GetPath();
-
-            //Common.FileList().ForEach(Console.WriteLine);
-
-            IpExtractor ex = new IpExtractor();
-            ex.ExtractIP();
-            APiHadler.ApiHandler();
-            List<string> hit = new List<string>();
-
-            using (StreamReader reader = new StreamReader("honeypot.txt"))
-            {
-                while (!reader.EndOfStream)
-                {
-                    string line = reader.ReadLine();
-                    if (line != null)
-                    {
-                        Honeypot.Add(line.Trim());
-                        Console.WriteLine(line); // Use line.
-                    }
-                }
-            }
-            List<string> nDistinct = IpExtractor.BlackList.Distinct().ToList();
-            Console.WriteLine("cout----{0}", nDistinct);
-            Console.WriteLine("=============================================================");
-            foreach (var item in nDistinct)
-            {
-                if (Honeypot.Contains(item))
-                {
-                    Console.WriteLine(item);
-                    hit.Add(item);
-                }
-                
-            }
-
-
-
-
             //---------------------File watcher----------------------
             //FileWatcher watch = new FileWatcher(path);
             //watch.CreateFileWatcher();
