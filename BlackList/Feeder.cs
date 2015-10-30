e669d34b98d16588e7b129a13555ac0a2b878a42
moimran <postme.imran@gmail.com>
2015-10-30 18:57:54 +0530

    Csv logic added

--- BlackList/Feeder.cs
+++ BlackList/Feeder.cs
@@ -16,18 +16,18 @@ namespace BlacklistNew
         Dictionary<string, List<KeyValuePair<string, List<string>>>> BlackListUnique = new Dictionary<string, List<KeyValuePair<string, List<string>>>>();
         internal List<IExtractor> FeedList = new List<IExtractor>();
         List<string> dup = new List<string>();
-        Ipset Ipsetdb = new Ipset();
+        //Ipset Ipsetdb = new Ipset();
         ShunList shunDb = new ShunList();
         BlockList_de BlockDb = new BlockList_de();
         SSLIpBlackList ccList = new SSLIpBlackList();
-        BadIp badip = new BadIp();
+        //BadIp badip = new BadIp();
         public void Runner()
         {
-            FeedList.Add(Ipsetdb);
+            //FeedList.Add(Ipsetdb);
             FeedList.Add(shunDb);
             FeedList.Add(BlockDb);
             FeedList.Add(ccList);
-            FeedList.Add(badip);
+            //FeedList.Add(badip);
             foreach (IExtractor item in FeedList)
             {
                 feeds(item);
@@ -77,24 +77,58 @@ namespace BlacklistNew
             }
             Console.WriteLine("ToTal Number of BlackList IP's = {0}", BlackListUnique.Count());
             Console.WriteLine("ToTal Number of duplicates IP's Found = {0}", dup.Count());
-            using (StreamWriter sw = File.CreateText("blacklistDB.csv"))
+ 
+            using (StreamWriter sw = File.CreateText("blacklistDB1.csv"))
             {
+                List<string> tempCat = new List<string>();
+                List<string> tempDesc = new List<string>();
+                List<string> tempDate = new List<string>();
+                List<string> tempUrl = new List<string>();
                 Console.WriteLine("Writing to File....");
                 foreach (KeyValuePair<string, List<KeyValuePair<string, List<string>>>> item in BlackListUnique)
                 {
+                    //Console.WriteLine("zzzzzzzzzzzzzzzzzzzzzzzzzzzzzz");
                     string ip = item.Key;
                     //Console.WriteLine("IP = {0}", ip);
                     for (int i = 0; i < item.Value.Count; i++)
                     {
+                        //Console.WriteLine("============================");
                         //Console.WriteLine("{0}", item.Value.ElementAt(i).Key);
                         foreach (var item2 in item.Value.ElementAt(i).Value)
                         {
+                            if (item.Value.ElementAt(i).Key == "category")
+                            {
+                                tempCat.Add(item2);
+                            }
                             //Console.WriteLine(item2);
-                            sw.WriteLine("{0},{1},{2}", ip, item.Value.ElementAt(i).Key, item2);
+                            if (item.Value.ElementAt(i).Key == "description")
+                            {
+                                tempDesc.Add(item2);
+                            }
+                            if (item.Value.ElementAt(i).Key == "url")
+                            {
+                                tempDate.Add(item2);
+                            }
+                            if (item.Value.ElementAt(i).Key == "date")
+                            {
+                                tempUrl.Add(item2);
+                            }
+                            
                         }
                     }
-                }
+                    string a = string.Join("  ", tempCat);
+                    string b = string.Join("  ", tempDesc);
+                    string c = string.Join("  ", tempDate);
+                    string d = string.Join("  ", tempUrl);
+                    //Console.WriteLine(ip + "," + a + "," + b + "," + c + "," + d);
+                    sw.WriteLine(ip + "," + a + "," + b + "," + c + "," + d);
+                    tempCat = new List<string>();
+                    tempDesc = new List<string>();
+                    tempDate = new List<string>();
+                    tempUrl = new List<string>();
 
+                }
+                Console.WriteLine("End Writing to File....");
             }
         }
 
